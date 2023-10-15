ca45b1d77346886aceb572efb132709508452c1e
Nishant khanderao <123276454+Khanderaonishant@users.noreply.github.com>
2023-10-16 02:21:07 +0530

    Update check for prime.cpp

--- check for prime.cpp
+++ check for prime.cpp
@@ -1,24 +1,16 @@
-
-#include <iostream>
-using namespace std;
-
-
-int main() {
-    int n;
-    int count=0;
-    cout<<"enter no to check for prime: "<<endl;
-    cin>>n;
-    for(int i=2;i<n;i++){
-        if(n%i==0)
-        count++;
-    }
-    if(count==1)
-    {
-        cout<<"False";
-    }
-    else
-    {
-        cout<<"true"<<endl;
-    }
-    return 0;
+bool isPrime(int n)
+{
+	
+	int count=0;
+	for(int i=1;i<n/2;i++)
+	{
+		if(n%i==0)
+		{
+			count++;
+		}
+	}
+	if(count==1)
+	return 1;
+	else
+	return 0;
 }
